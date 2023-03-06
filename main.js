06906d8fb1a3a08e440465cc8e07fc9f39d08b12
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-06 09:34:25 -0300

    Adicionando comentários :)

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+// Importando algumas propriedades do electron
 const { app, ipcMain } = require("electron");
 
 const appEvents = require("./events/app");
 
+/**
+ * "Ouvindo" os eventos de (app)
+ */
 appEvents.forEach((ev) => {
 	app.on(ev.name, (...args) => {
 		ev.execute(...args)
@@ -10,14 +14,11 @@ appEvents.forEach((ev) => {
 
 const ipcMainEvents = require("./events/ipcMain");
 
+/**
+ * Ouvindo os eventos de (ipcMain)
+ */
 ipcMainEvents.forEach((ev) => {
 	ipcMain.on(ev.name, (...args) => {
 		ev.execute(...args);
 	});
-});
-
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
 });
