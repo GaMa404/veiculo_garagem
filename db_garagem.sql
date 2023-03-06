646bd813cc72528002b60f10f77d6e39dd3d7488
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-06 09:34:06 -0300

    Alimentando o banco de dados.

--- db_garagem.sql
+++ db_garagem.sql
@@ -47,3 +47,30 @@ CREATE TABLE veiculo
     FOREIGN KEY (id_tipo) REFERENCES tipo(id),
     FOREIGN KEY (id_combustivel) REFERENCES combustivel(id)
 );
+
+INSERT INTO Marca(descricao, fabricante) VALUES('Toyota', 'Toyota Motor Corporation');
+INSERT INTO Marca(descricao, fabricante) VALUES('Ford', 'Ford Motor Company');
+INSERT INTO Marca(descricao, fabricante) VALUES('Chevrolet', 'General Motors');
+INSERT INTO Marca(descricao, fabricante) VALUES('Honda', 'Honda Motor Company');
+INSERT INTO Marca(descricao, fabricante) VALUES('Nissan', 'Nissan Motor Company');
+INSERT INTO Marca(descricao, fabricante) VALUES('BMW', 'Bayerische Motoren Werke AG');
+INSERT INTO Marca(descricao, fabricante) VALUES('Mercedes-Benz', 'Daimler AG');
+INSERT INTO Marca(descricao, fabricante) VALUES('Audi', 'Volkswagen Group');
+INSERT INTO Marca(descricao, fabricante) VALUES('Hyundai', 'Hyundai Motor Company');
+INSERT INTO Marca(descricao, fabricante) VALUES('Kia', 'Kia Corporation');
+
+INSERT INTO Combustivel(descricao) VALUES('Gasolina');
+INSERT INTO Combustivel(descricao) VALUES('Etanol');
+INSERT INTO Combustivel(descricao) VALUES('Diesel');
+INSERT INTO Combustivel(descricao) VALUES('GNV');
+INSERT INTO Combustivel(descricao) VALUES('Biodiesel');
+INSERT INTO Combustivel(descricao) VALUES('Hidrogênio');
+INSERT INTO Combustivel(descricao) VALUES('Eletricidade');
+
+INSERT INTO Tipo(descricao) VALUES('Sedan');
+INSERT INTO Tipo(descricao) VALUES('Hatch');
+INSERT INTO Tipo(descricao) VALUES('SUV');
+INSERT INTO Tipo(descricao) VALUES('Pick-up');
+INSERT INTO Tipo(descricao) VALUES('Coupé');
+INSERT INTO Tipo(descricao) VALUES('Van');
+INSERT INTO Tipo(descricao) VALUES('Esportivo');
